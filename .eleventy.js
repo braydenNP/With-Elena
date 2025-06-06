9698d8e987b6c43a0bce027ba2bdd4883ae5af04
braydenNP <s10258659@connect.np.edu.sg>
2025-06-06 19:06:01 +0800

    withe elena

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = async function(eleventyConfig) {
+	// Configure Eleventy
+    eleventyConfig.addPassthroughCopy("./src/css/");
+    eleventyConfig.addWatchTarget("./src/css/");
+    eleventyConfig.addPassthroughCopy("./src/js/");
+    eleventyConfig.addPassthroughCopy("./src/admin/");
+    eleventyConfig.addPassthroughCopy("./src/images/");
+    eleventyConfig.addPassthroughCopy("./src/styles.css");
+    eleventyConfig.addPassthroughCopy("./src/*/styles.css");
+    return {
+        dir: {
+            input: "src",
+            output: "public",
+        },
+    };
+};
---
+++ README copy.md
@@ -0,0 +1,4 @@
+# website-template from codestitch
+
+This is what I assume to be the final step before going out and cold calling potential clients. A template so I can get started on projects quickly.
+A base layout, with a navbar, a footer. Properly created. 
