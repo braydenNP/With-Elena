22c420a8c8789183401b541c05453ec8eb26b213
braydenNP <s10258659@connect.np.edu.sg>
2025-06-19 16:12:22 +0800

    hi

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 module.exports = async function(eleventyConfig) {
 	// Configure Eleventy
     eleventyConfig.addPassthroughCopy("./src/css/");
@@ -8,6 +9,9 @@ module.exports = async function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/fonts/");
     eleventyConfig.addPassthroughCopy("./src/styles.css");
     eleventyConfig.addPassthroughCopy("./src/*/styles.css");
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    }) 
     return {
         dir: {
             input: "src",
